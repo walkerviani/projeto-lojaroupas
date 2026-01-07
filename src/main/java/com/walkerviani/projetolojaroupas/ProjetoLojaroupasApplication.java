895cc9dd6b7a967afb866978ac80bccdc09ecf89
Walker Viani <walkeryslan@gmail.com>
2026-01-07 20:04:48 -0300

    project created

---
+++ src/main/java/com/walkerviani/projetolojaroupas/ProjetoLojaroupasApplication.java
@@ -0,0 +1,13 @@
+package com.walkerviani.projetolojaroupas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoLojaroupasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoLojaroupasApplication.class, args);
+    }
+
+}
