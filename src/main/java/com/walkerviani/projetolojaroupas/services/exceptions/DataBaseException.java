1e557a64f690e1a8887a71425309afd3fc33211c
Walker Viani <walkeryslan@gmail.com>
2026-01-08 18:20:20 -0300

    "Database Exception"

---
+++ src/main/java/com/walkerviani/projetolojaroupas/services/exceptions/DataBaseException.java
@@ -0,0 +1,9 @@
+package com.walkerviani.projetolojaroupas.services.exceptions;
+
+public class DataBaseException extends RuntimeException {
+
+
+    public DataBaseException(String message) {
+        super(message);
+    }
+}
