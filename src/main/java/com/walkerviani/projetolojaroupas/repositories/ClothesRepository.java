0894aa530cfc11a691de25edf264362ccad0df5d
Walker Viani <walkeryslan@gmail.com>
2026-01-08 18:32:05 -0300

    "Clothes Service and DatabaseException fix"

--- src/main/java/com/walkerviani/projetolojaroupas/repositories/ClothesRepository.java
+++ src/main/java/com/walkerviani/projetolojaroupas/repositories/ClothesRepository.java
@@ -3,5 +3,6 @@ package com.walkerviani.projetolojaroupas.repositories;
 import com.walkerviani.projetolojaroupas.entities.Clothes;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 public interface ClothesRepository extends JpaRepository<Clothes, Long> {
 }
---
+++ src/main/java/com/walkerviani/projetolojaroupas/services/ClothesService.java
@@ -0,0 +1,64 @@
+package com.walkerviani.projetolojaroupas.services;
+
+import com.walkerviani.projetolojaroupas.entities.Clothes;
+import com.walkerviani.projetolojaroupas.repositories.ClothesRepository;
+import com.walkerviani.projetolojaroupas.services.exceptions.ClothesNotFoundException;
+import com.walkerviani.projetolojaroupas.services.exceptions.DatabaseException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ClothesService {
+
+    @Autowired
+    private ClothesRepository clothesRepository;
+
+    public List<Clothes> findAll() {
+        return clothesRepository.findAll();
+    }
+
+    public Clothes findById(Long id) {
+        Optional<Clothes> clothes = clothesRepository.findById(id);
+        return clothes.orElseThrow(() -> new ClothesNotFoundException(id));
+    }
+
+    public Clothes insert(Clothes obj) {
+        return clothesRepository.save(obj);
+    }
+
+    public void delete(Long id) {
+        try {
+            clothesRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ClothesNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+
+    public Clothes update(Long id, Clothes obj) {
+        try {
+            Clothes entity = clothesRepository.getReferenceById(id);
+            updateData(entity, obj);
+            return clothesRepository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ClothesNotFoundException(id);
+        }
+
+    }
+
+    private void updateData(Clothes entity, Clothes obj) {
+        entity.setName(obj.getName());
+        entity.setPrice(obj.getPrice());
+        entity.setDescription(obj.getDescription());
+        entity.setImageUrl(obj.getImageUrl());
+        entity.setSize(obj.getSize());
+        entity.setImageUrl(obj.getImageUrl());
+    }
+}
