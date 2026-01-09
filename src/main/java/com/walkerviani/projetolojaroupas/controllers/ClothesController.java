0bd9a12c07c8d8938a518b990404a616145fba4a
Walker Viani <walkeryslan@gmail.com>
2026-01-09 19:33:30 -0300

    "ClothesController and fix ClothesRepository"

---
+++ src/main/java/com/walkerviani/projetolojaroupas/controllers/ClothesController.java
@@ -0,0 +1,64 @@
+package com.walkerviani.projetolojaroupas.controllers;
+
+import com.walkerviani.projetolojaroupas.entities.Clothes;
+import com.walkerviani.projetolojaroupas.entities.enums.Color;
+import com.walkerviani.projetolojaroupas.services.ClothesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/clothes")
+public class ClothesController {
+
+    @Autowired
+    private ClothesService clothesService;
+
+    @GetMapping
+    public ResponseEntity<List<Clothes>> findAll() {
+        return ResponseEntity.ok().body(clothesService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Clothes> findById(@PathVariable Long id) {
+        Clothes obj = clothesService.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping("/name")
+    public ResponseEntity<List<Clothes>> findByName(@RequestParam(value = "name", required = false) String name) {
+        List<Clothes> list = (name == null || name.isBlank())
+                ? clothesService.findAll()
+                : clothesService.findByName(name);
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping("/color")
+    public ResponseEntity<List<Clothes>> findByColor(@RequestParam(value = "color", required = false) Color color) {
+        List<Clothes> list = clothesService.findByColor(color);
+        return ResponseEntity.ok().body(list);
+    }
+
+    @PostMapping
+    public ResponseEntity<Clothes> insert(@RequestBody Clothes obj) {
+        obj = clothesService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        clothesService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Clothes> update(@PathVariable Long id, @RequestBody Clothes obj) {
+        obj = clothesService.update(id, obj);
+        return ResponseEntity.ok().body(obj);
+    }
+}
