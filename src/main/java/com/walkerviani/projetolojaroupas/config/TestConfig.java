c772730e677c7dd54bb5b62a5f4f4459f8d9a8dc
Walker Viani <walkeryslan@gmail.com>
2026-01-10 14:17:37 -0300

    "h2 database testing"

---
+++ src/main/java/com/walkerviani/projetolojaroupas/config/TestConfig.java
@@ -0,0 +1,32 @@
+package com.walkerviani.projetolojaroupas.config;
+
+import com.walkerviani.projetolojaroupas.entities.Clothes;
+import com.walkerviani.projetolojaroupas.entities.enums.Category;
+import com.walkerviani.projetolojaroupas.entities.enums.Color;
+import com.walkerviani.projetolojaroupas.entities.enums.Size;
+import com.walkerviani.projetolojaroupas.repositories.ClothesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.Arrays;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+
+    @Autowired
+    private ClothesRepository clothesRepository;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Clothes clothes1 = new Clothes(null, "Red T-shirt", 20.33, "100% Cotton", "/images/image1.png", Size.MEDIUM, Category.TSHIRT, Color.RED);
+        Clothes clothes2 = new Clothes(null, "Black skirt", 12.42, "45% Cotton, 55% Polyester", "/images/image2.png", Size.SMALL, Category.SKIRT, Color.BLACK);
+        Clothes clothes3 = new Clothes(null, "Blue coat", 45.53, "90% Cotton, 10% wool", "/images/image3.png", Size.LARGE, Category.COAT, Color.BLUE);
+
+        clothesRepository.saveAll(Arrays.asList(clothes1,clothes2,clothes3));
+    }
+}
