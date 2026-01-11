de7ff53d97729de06d1535eca83823f2430727c0
Walker Viani <walkeryslan@gmail.com>
2026-01-11 19:20:40 -0300

    "added addStock and removeStock"

--- src/main/java/com/walkerviani/projetolojaroupas/controllers/ClothesController.java
+++ src/main/java/com/walkerviani/projetolojaroupas/controllers/ClothesController.java
@@ -56,6 +56,18 @@ public class ClothesController {
         return ResponseEntity.ok().body(list);
     }
 
+    @PatchMapping("/{id}/add-stock")
+    public ResponseEntity<Void> addStock(@PathVariable Long id, @RequestParam int amount){
+        clothesService.addStock(id, amount);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/{id}/remove-stock")
+    public ResponseEntity<Void> removeStock(@PathVariable Long id, @RequestParam int amount){
+        clothesService.removeStock(id, amount);
+        return ResponseEntity.noContent().build();
+    }
+
     @GetMapping("/{id}/stock")
     public ResponseEntity<Boolean> isStockAvailable(@PathVariable Long id,@RequestParam int  quantity) {
         boolean available = clothesService.isStockAvailable(id, quantity);
