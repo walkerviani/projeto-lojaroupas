b6ab8a53bdf6699948d27c3745c39b7bb2713a49
Walker Viani <walkeryslan@gmail.com>
2026-01-11 18:46:31 -0300

    "Updated how Category is instantiated"

--- src/main/java/com/walkerviani/projetolojaroupas/config/TestConfig.java
+++ src/main/java/com/walkerviani/projetolojaroupas/config/TestConfig.java
@@ -1,7 +1,7 @@
 package com.walkerviani.projetolojaroupas.config;
 
+import com.walkerviani.projetolojaroupas.entities.Category;
 import com.walkerviani.projetolojaroupas.entities.Clothes;
-import com.walkerviani.projetolojaroupas.entities.enums.Category;
 import com.walkerviani.projetolojaroupas.entities.enums.Color;
 import com.walkerviani.projetolojaroupas.entities.enums.Size;
 import com.walkerviani.projetolojaroupas.repositories.ClothesRepository;
@@ -24,9 +24,9 @@ public class TestConfig implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        Clothes clothes1 = new Clothes(null, "Red T-shirt", new BigDecimal("20.33"), "100% Cotton", "/images/image1.png", Size.MEDIUM, Category.TSHIRT, Color.RED, 5);
-        Clothes clothes2 = new Clothes(null, "Black skirt", new BigDecimal("12.42"), "45% Cotton, 55% Polyester", "/images/image2.png", Size.SMALL, Category.SKIRT, Color.BLACK, 8);
-        Clothes clothes3 = new Clothes(null, "Blue coat", new BigDecimal("45.43"), "90% Cotton, 10% wool", "/images/image3.png", Size.LARGE, Category.COAT, Color.BLUE, 9);
+        Clothes clothes1 = new Clothes(null, "Red T-shirt", new BigDecimal("20.33"), "100% Cotton", "/images/image1.png", Size.MEDIUM, new Category("T-shirt"), Color.RED, 5);
+        Clothes clothes2 = new Clothes(null, "Black skirt", new BigDecimal("12.42"), "45% Cotton, 55% Polyester", "/images/image2.png", Size.SMALL, new Category("Skirt"), Color.BLACK, 8);
+        Clothes clothes3 = new Clothes(null, "Blue coat", new BigDecimal("45.43"), "90% Cotton, 10% wool", "/images/image3.png", Size.LARGE, new Category("Coat"), Color.BLUE, 9);
 
         clothesRepository.saveAll(Arrays.asList(clothes1, clothes2, clothes3));
     }
