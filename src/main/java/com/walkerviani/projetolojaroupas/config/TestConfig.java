a6c378170bdac225c344b61ba33c35739bb75758
Walker Viani <walkeryslan@gmail.com>
2026-01-12 16:02:25 -0300

    "added categories"

--- src/main/java/com/walkerviani/projetolojaroupas/config/TestConfig.java
+++ src/main/java/com/walkerviani/projetolojaroupas/config/TestConfig.java
@@ -4,6 +4,7 @@ import com.walkerviani.projetolojaroupas.entities.Category;
 import com.walkerviani.projetolojaroupas.entities.Clothes;
 import com.walkerviani.projetolojaroupas.entities.enums.Color;
 import com.walkerviani.projetolojaroupas.entities.enums.Size;
+import com.walkerviani.projetolojaroupas.repositories.CategoryRepository;
 import com.walkerviani.projetolojaroupas.repositories.ClothesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -20,14 +21,22 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private ClothesRepository clothesRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Override
     public void run(String... args) throws Exception {
 
-        Clothes clothes1 = new Clothes("Red T-shirt", new BigDecimal("20.33"), "100% Cotton", "/images/image1.png", Size.MEDIUM, new Category("T-shirt"), Color.RED, 5);
-        Clothes clothes2 = new Clothes("Black skirt", new BigDecimal("12.42"), "45% Cotton, 55% Polyester", "/images/image2.png", Size.SMALL, new Category("Skirt"), Color.BLACK, 8);
-        Clothes clothes3 = new Clothes("Blue coat", new BigDecimal("45.43"), "90% Cotton, 10% wool", "/images/image3.png", Size.LARGE, new Category("Coat"), Color.BLUE, 9);
+        Category cat1 = new Category("Tshirt");
+        Category cat2 = new Category("Skirt");
+        Category cat3 = new Category("Coat");
 
+        Clothes clothes1 = new Clothes("Red Tshirt", new BigDecimal("20.33"), "100% Cotton", "/images/image1.png", Size.MEDIUM, cat1, Color.RED, 5);
+        Clothes clothes2 = new Clothes("Black skirt", new BigDecimal("12.42"), "45% Cotton, 55% Polyester", "/images/image2.png", Size.SMALL, cat2, Color.BLACK, 8);
+        Clothes clothes3 = new Clothes("Blue coat", new BigDecimal("45.43"), "90% Cotton, 10% wool", "/images/image3.png", Size.LARGE, cat3, Color.BLUE, 9);
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
         clothesRepository.saveAll(Arrays.asList(clothes1, clothes2, clothes3));
+
     }
 }
