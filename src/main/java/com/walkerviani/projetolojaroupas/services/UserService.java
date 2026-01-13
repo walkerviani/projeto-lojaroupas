46a11fe3041acb221d0b3da56c9a3cbb1bab27fc
Walker Viani <walkeryslan@gmail.com>
2026-01-13 08:45:37 -0300

    "User service and UserNotFounException"

---
+++ src/main/java/com/walkerviani/projetolojaroupas/services/UserService.java
@@ -0,0 +1,70 @@
+package com.walkerviani.projetolojaroupas.services;
+
+import com.walkerviani.projetolojaroupas.entities.User;
+import com.walkerviani.projetolojaroupas.repositories.UserRepository;
+import com.walkerviani.projetolojaroupas.services.exceptions.DatabaseException;
+import com.walkerviani.projetolojaroupas.services.exceptions.UserNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = userRepository.findById(id);
+        return obj.orElseThrow(() -> new UserNotFoundException("User not found"));
+    }
+
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public User insert(User obj) {
+        return userRepository.save(obj);
+    }
+
+    public void delete(Long id) {
+        try {
+            userRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new UserNotFoundException("User not found");
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+
+    public User update(Long id, User obj) {
+        try {
+            User entity = userRepository.getReferenceById(id);
+            updateData(entity, obj);
+            return userRepository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new UserNotFoundException("User not found");
+        }
+    }
+
+    private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
+    }
+
+    public User updatePassword(Long id, String newPassword) {
+        User entity = userRepository.getReferenceById(id);
+        entity.setPassword(newPassword);
+        userRepository.save(entity);
+    }
+}
