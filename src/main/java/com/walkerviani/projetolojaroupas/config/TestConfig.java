05100afec4b402bbf2915eb8ad377de1034dd62c
Walker Viani <walkeryslan@gmail.com>
2026-01-15 18:50:47 -0300

    "Order table test"

--- src/main/java/com/walkerviani/projetolojaroupas/config/TestConfig.java
+++ src/main/java/com/walkerviani/projetolojaroupas/config/TestConfig.java
@@ -2,16 +2,20 @@ package com.walkerviani.projetolojaroupas.config;
 
 import com.walkerviani.projetolojaroupas.entities.Category;
 import com.walkerviani.projetolojaroupas.entities.Clothes;
+import com.walkerviani.projetolojaroupas.entities.Order;
 import com.walkerviani.projetolojaroupas.entities.enums.Color;
+import com.walkerviani.projetolojaroupas.entities.enums.OrderStatus;
 import com.walkerviani.projetolojaroupas.entities.enums.Size;
 import com.walkerviani.projetolojaroupas.repositories.CategoryRepository;
 import com.walkerviani.projetolojaroupas.repositories.ClothesRepository;
+import com.walkerviani.projetolojaroupas.repositories.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import java.math.BigDecimal;
+import java.time.Instant;
 import java.util.Arrays;
 
 @Configuration
@@ -24,6 +28,9 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Autowired
+    private OrderRepository orderRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
@@ -35,8 +42,13 @@ public class TestConfig implements CommandLineRunner {
         Clothes clothes2 = new Clothes("Black skirt", new BigDecimal("12.42"), "45% Cotton, 55% Polyester", "/images/image2.png", Size.SMALL, cat2, Color.BLACK, 8);
         Clothes clothes3 = new Clothes("Blue coat", new BigDecimal("45.43"), "90% Cotton, 10% wool", "/images/image3.png", Size.LARGE, cat3, Color.BLUE, 9);
 
+
+        Order ord1 = new Order(Instant.now(), OrderStatus.PAID);
+        Order ord2 = new Order(Instant.now(), OrderStatus.WAITING_PAYMENT);
+
+
         categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
         clothesRepository.saveAll(Arrays.asList(clothes1, clothes2, clothes3));
-
+        orderRepository.saveAll(Arrays.asList(ord1, ord2));
     }
 }
