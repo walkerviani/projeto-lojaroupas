d4ec43207ca495704dab3f8ae5bcb9612b7ae0e4
Walker Viani <walkeryslan@gmail.com>
2026-01-15 19:21:53 -0300

    "OrderItem table"

---
+++ src/main/java/com/walkerviani/projetolojaroupas/entities/OrderItem.java
@@ -0,0 +1,80 @@
+package com.walkerviani.projetolojaroupas.entities;
+
+import com.walkerviani.projetolojaroupas.entities.pk.OrderItemPK;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Entity
+@Table(name = "order_item")
+public class OrderItem implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @EmbeddedId
+    private OrderItemPK id = new OrderItemPK();
+    private int quantity;
+    private BigDecimal price;
+
+    public OrderItem() {
+
+    }
+
+    public OrderItem(Order order, Clothes clothes, int quantity, BigDecimal price) {
+        id.setOrder(order);
+        id.setClothes(clothes);
+        this.quantity = quantity;
+        this.price = price;
+
+    }
+
+    public Order getOrder() {
+        return id.getOrder();
+    }
+
+    public void setOrder(Order order) {
+        id.setOrder(order);
+    }
+
+    public Clothes getClothes() {
+        return id.getClothes();
+    }
+
+    public void setClothes(Clothes product) {
+        id.setClothes(product);
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItem orderItem = (OrderItem) o;
+        return Objects.equals(id, orderItem.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
---
+++ src/main/java/com/walkerviani/projetolojaroupas/entities/pk/OrderItemPK.java
@@ -0,0 +1,51 @@
+package com.walkerviani.projetolojaroupas.entities.pk;
+
+import com.walkerviani.projetolojaroupas.entities.Clothes;
+import com.walkerviani.projetolojaroupas.entities.Order;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+public class OrderItemPK implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "clothes_id")
+    private Clothes clothes;
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Clothes getClothes() {
+        return clothes;
+    }
+
+    public void setClothes(Clothes clothes) {
+        this.clothes = clothes;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItemPK that = (OrderItemPK) o;
+        return Objects.equals(order, that.order) && Objects.equals(clothes, that.clothes);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(order, clothes);
+    }
+}
