80859f704b6dcc93587f5e193ad46ed765f6032a
Walker Viani <walkeryslan@gmail.com>
2026-01-16 19:14:39 -0300

    "implemented association with clothes"

--- src/main/java/com/walkerviani/projetolojaroupas/entities/Category.java
+++ src/main/java/com/walkerviani/projetolojaroupas/entities/Category.java
@@ -4,7 +4,9 @@ import jakarta.persistence.*;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "category")
@@ -17,6 +19,10 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @OneToMany
+    @JoinColumn(name = "clothes_id")
+    private Set<Clothes> items = new HashSet<>();
 
     public Category() {
     }
@@ -35,6 +41,10 @@ public class Category implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Set<Clothes> getItems() {
+        return items;
     }
 
     @Override
