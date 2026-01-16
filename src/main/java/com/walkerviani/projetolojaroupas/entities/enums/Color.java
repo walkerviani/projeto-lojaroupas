19afe6ffdf8c9b3448606e1c6f5893aaf5b00744
Walker Viani <walkeryslan@gmail.com>
2026-01-16 18:32:52 -0300

    "improvements to enums"

--- src/main/java/com/walkerviani/projetolojaroupas/entities/enums/Color.java
+++ src/main/java/com/walkerviani/projetolojaroupas/entities/enums/Color.java
@@ -1,15 +1,34 @@
 package com.walkerviani.projetolojaroupas.entities.enums;
 
 public enum Color {
-    RED,
-    YELLOW,
-    BLUE,
-    GREEN,
-    ORANGE,
-    PURPLE,
-    WHITE,
-    BLACK,
-    GREY,
-    BEIGE,
-    BROWN;
+    RED(1),
+    YELLOW(2),
+    BLUE(3),
+    GREEN(4),
+    ORANGE(5),
+    PURPLE(6),
+    WHITE(7),
+    BLACK(8),
+    GREY(9),
+    BEIGE(10),
+    BROWN(11);
+
+    private int code;
+
+    private Color(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public static Color valueOf(int code) {
+        for (Color value : Color.values()) {
+            if (value.getCode() == code) {
+                return value;
+            }
+        }
+        throw new IllegalArgumentException("Invalid Color code");
+    }
 }
--- src/main/java/com/walkerviani/projetolojaroupas/entities/enums/Size.java
+++ src/main/java/com/walkerviani/projetolojaroupas/entities/enums/Size.java
@@ -1,7 +1,26 @@
 package com.walkerviani.projetolojaroupas.entities.enums;
 
 public enum Size {
-    SMALL,
-    MEDIUM,
-    LARGE;
+    SMALL(1),
+    MEDIUM(2),
+    LARGE(3);
+
+    private int code;
+
+    private Size(int code){
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public static Size valueOf(int code) {
+        for (Size value : Size.values()) {
+            if (value.getCode() == code) {
+                return value;
+            }
+        }
+        throw new IllegalArgumentException("Invalid Size code");
+    }
 }
