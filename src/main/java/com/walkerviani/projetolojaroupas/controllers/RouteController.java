4e7bd89c4c553c9d7aba3a92ec6ef0fa020d7cee
Walker Viani <walkeryslan@gmail.com>
2026-02-02 19:03:43 -0300

    added route controller

---
+++ src/main/java/com/walkerviani/projetolojaroupas/controllers/RouteController.java
@@ -0,0 +1,13 @@
+package com.walkerviani.projetolojaroupas.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RouteController {
+
+    @RequestMapping(value = "/{path:[^\\.]*}")
+    public String forward(){
+        return "forward:/index.html";
+    }
+}
