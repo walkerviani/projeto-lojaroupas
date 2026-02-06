02d9e0f5ef0cad46442b6c106107068e33b7fd95
Walker Viani <walkeryslan@gmail.com>
2026-02-06 16:16:27 -0300

    added cpf column

--- src/main/java/com/walkerviani/projetolojaroupas/controllers/UserController.java
+++ src/main/java/com/walkerviani/projetolojaroupas/controllers/UserController.java
@@ -1,15 +1,26 @@
 package com.walkerviani.projetolojaroupas.controllers;
 
-import com.walkerviani.projetolojaroupas.entities.User;
-import com.walkerviani.projetolojaroupas.services.UserService;
+import java.net.URI;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
-import java.util.List;
+import com.walkerviani.projetolojaroupas.entities.User;
+import com.walkerviani.projetolojaroupas.services.UserService;
+
 
 @RestController
 @RequestMapping(value = "/users")
@@ -30,10 +41,21 @@ public class UserController {
     }
 
     @GetMapping("/email")
-    public ResponseEntity<User> findByEmail(@RequestParam(value = "email") String email) {
+    public ResponseEntity<User> findByEmail(@RequestParam String email) {
         User obj = userService.findByEmail(email);
         return ResponseEntity.ok().body(obj);
     }
+
+    @GetMapping("/cpf")
+    public ResponseEntity<User> findByCpf(@RequestParam String cpf){
+        User obj = userService.findByCpf(cpf);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    public String getMethodName(@RequestParam String param) {
+        return new String();
+    }
+    
 
     @PostMapping
     public ResponseEntity<User> insert(@RequestBody User obj) {
--- src/main/java/com/walkerviani/projetolojaroupas/repositories/UserRepository.java
+++ src/main/java/com/walkerviani/projetolojaroupas/repositories/UserRepository.java
@@ -1,8 +1,10 @@
 package com.walkerviani.projetolojaroupas.repositories;
 
-import com.walkerviani.projetolojaroupas.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.walkerviani.projetolojaroupas.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
+    User findByCpf(String cpf);
 }
