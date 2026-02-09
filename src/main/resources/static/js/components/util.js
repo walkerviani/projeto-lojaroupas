d3f99ccbd7493c58b64e2f7bf63c273d3d1b740f
Walker Viani <walkeryslan@gmail.com>
2026-02-09 14:02:44 -0300

    added admin products table

--- src/main/resources/static/js/components/util.js
+++ src/main/resources/static/js/components/util.js
@@ -13,6 +13,32 @@ export async function getProducts(url) {
     }
 }
 
+export function showProductTable(products) {
+    let table = `
+    <tr>
+        <td>ID</td>
+        <td>Name</td>
+        <td>Price</td>
+        <td>Description</td>
+        <td>Size</td>
+        <td>Color</td>
+    </tr>`;
+
+    for(let product of products){
+        table += `
+        <tr>
+          <td>${product.id}</td>
+          <td>${product.name}</td>
+          <td>${product.price}</td>
+          <td>${product.description}</td>
+          <td>${product.size}</td>
+          <td>${product.color}</td>
+        </tr>
+        `;
+    }
+    document.getElementById('product-table').innerHTML = table;
+}
+
 export function capitalizeFirstLetter(text) {
     const str = text.toLowerCase();
     return str.charAt(0).toUpperCase() + str.slice(1);
