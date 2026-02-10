6673e68029a03baccc8a9271a59dfcd533a3b497
Walker Viani <walkeryslan@gmail.com>
2026-02-10 12:42:59 -0300

    added imageData entity, service, controller and repository

---
+++ src/main/java/com/walkerviani/projetolojaroupas/entities/ImageData.java
@@ -0,0 +1,38 @@
+package com.walkerviani.projetolojaroupas.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name= "ImageData")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ImageData {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String type;
+    @Lob
+    @Column(name= "imagedata")
+    @JsonIgnore
+    private byte[] imageData;
+    
+    @JsonIgnore
+    @OneToOne(mappedBy= "imageData")
+    private Clothes product;
+}
