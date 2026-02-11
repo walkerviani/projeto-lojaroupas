11da215a81f549b40e2e7acb2425a23bc0f7a45b
Walker Viani <walkeryslan@gmail.com>
2026-02-11 20:08:41 -0300

    implement category creation validation and routing; refactor product-related functions for consistency

--- src/main/resources/static/js/components/form-validations.js
+++ src/main/resources/static/js/components/form-validations.js
@@ -23,7 +23,7 @@ export function validateCreateAccount() {
             error = "Name must not have numbers";
         } else if (cpf.validity.valueMissing) { // Cpf validation
             error = "CPF is required!";
-        } else if(cpf.value.trim().length < 11){
+        } else if (cpf.value.trim().length < 11) {
             error = "CPF must have 11 digits";
         } else if (email.validity.valueMissing) { // Email validation
             error = "Email is required!";
@@ -68,7 +68,9 @@ async function sendAccountData(userData, alert, form) {
 
         const response = await fetch(`${BASE_URL}/users`, {
             method: "POST",
-            headers: { "Content-Type": "application/json" },
+            headers: {
+                "Content-Type": "application/json"
+            },
             body: JSON.stringify(userData)
         });
 
@@ -89,10 +91,10 @@ async function sendAccountData(userData, alert, form) {
     }
 }
 
-export function validateCreateProducts(){
+export function validateCreateProducts() {
     const form = document.getElementById('form-create-product');
     const { name, price, description, size, color, category, image } = form.elements;
-    const alert = document.getElementById('alert-create-prod');
+    const alert = document.getElementById('alert-create-product');
 
     form.addEventListener('submit', async (e) => {
         e.preventDefault();
@@ -103,28 +105,27 @@ export function validateCreateProducts(){
         // Reset the label and error
         alert.textContent = "";
         let error = "";
-
         if (name.validity.valueMissing) {
             error = "Name is required!";
         } else if (name.validity.tooShort) {
             error = "Name is too short!";
         } else if (!nameRegex.test(name.value)) {
             error = "Name must not have numbers";
-        } else if (price.validity.valueMissing){
+        } else if (price.validity.valueMissing) {
             error = "Price is required!";
-        } else if(isNaN(price)) {
+        } else if (isNaN(price.value)) {
             error = "Enter a valid price!";
         } else if (description.validity.valueMissing) {
             error = "Description is required!";
-        } else if (size.value === ""){
+        } else if (size.value === "") {
             error = "You must select a valid size!";
-        } else if (color.value === ""){
+        } else if (color.value === "") {
             error = "You must select a valid color!";
-        } else if (category.value === ""){
+        } else if (category.value === "") {
             error = "You must select a valid category!";
         } else if (image.validity.valueMissing) {
             error = "You must select an image";
-        } else if(file && file.type !== "image/png"){
+        } else if (file && file.type !== "image/png") {
             error = "Only PNG images are allowed!";
         }
 
@@ -138,8 +139,8 @@ export function validateCreateProducts(){
                 description: description.value,
                 size: size.value,
                 color: color.value,
-                category: { 
-                    id: category.value 
+                category: {
+                    id: category.value
                 },
                 imageName: ""
             };
@@ -148,8 +149,8 @@ export function validateCreateProducts(){
     });
 }
 
-async function sendProductData(productData, file, alert, form){
-    try{
+async function sendProductData(productData, file, alert, form) {
+    try {
         //image upload
         const imageFormData = new FormData();
         imageFormData.append('image', file);
@@ -159,7 +160,7 @@ async function sendProductData(productData, file, alert, form){
             body: imageFormData
         });
 
-        if(!imageResponse.ok) throw new Error("Failed to upload image");
+        if (!imageResponse.ok) throw new Error("Failed to upload image");
         const imageResultText = await imageResponse.text();
         const fileName = imageResultText.split(": ")[1];
 
@@ -168,12 +169,12 @@ async function sendProductData(productData, file, alert, form){
         const productResponse = await fetch(`${BASE_URL}/clothes`, {
             method: 'POST',
             headers: {
-                'Content-Type' : 'application/json'
+                'Content-Type': 'application/json'
             },
             body: JSON.stringify(productData)
         });
 
-        if (productResponse.ok){
+        if (productResponse.ok) {
             alert.scrollIntoView({ behavior: "smooth", block: "center" });
             alert.style.color = "green";
             alert.textContent = "Product created successfully!";
@@ -181,7 +182,66 @@ async function sendProductData(productData, file, alert, form){
         } else {
             throw new Error("Failed to create");
         }
-    } catch (error){
+    } catch (error) {
+        alert.scrollIntoView({ behavior: "smooth", block: "center" });
+        alert.style.color = "red";
+        alert.textContent = error.message;
+    }
+}
+
+export function validateCreateCategories() {
+    const form = document.getElementById('form-create-category');
+    const name = document.getElementById('name-create-category');
+    const alert = document.getElementById('alert-create-categ');
+
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        const nameRegex = /^[A-Za-zÀ-ÿ\s]+$/; //allow normal and accented characters and whitespace
+
+        // Reset the label and error
+        alert.textContent = "";
+        let error = "";
+
+        if (name.validity.valueMissing) {
+            error = "Name is required!";
+        } else if (name.validity.tooShort) {
+            error = "Name is too short!";
+        } else if (!nameRegex.test(name.value)) {
+            error = "Name must not have numbers";
+        }
+
+        if (error != "") {
+            alert.textContent = error;
+            alert.scrollIntoView({ behavior: "smooth", block: "center" });
+        } else {
+            const categoryData = {
+                name: name.value,
+                
+            };
+            await sendCategoryData(categoryData, alert, form);
+        }
+    });
+}
+
+async function sendCategoryData(categoryData, alert, form) {
+    try {
+        const categoryResponse = await fetch(`${BASE_URL}/category`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(categoryData)
+        });
+
+        if (categoryResponse.ok) {
+            alert.scrollIntoView({ behavior: "smooth", block: "center" });
+            alert.style.color = "green";
+            alert.textContent = "Category created successfully!";
+            form.reset();
+        } else {
+            throw new Error("Failed to create");
+        }
+    } catch (error) {
         alert.scrollIntoView({ behavior: "smooth", block: "center" });
         alert.style.color = "red";
         alert.textContent = error.message;
