aa62d029a69cbbf9d3859d2fd285d4a3ed7ee152
Walker Viani <walkeryslan@gmail.com>
2026-03-01 18:16:10 -0300

    add authentication file

---
+++ src/main/resources/static/js/services/auth.js
@@ -0,0 +1,53 @@
+import { BASE_URL } from "../components/util.js";
+
+export async function authenticateUser(obj) {
+    try {
+        const response = await fetch(`${BASE_URL}/auth/login`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(obj),
+            credentials: "include"
+        });
+        const message = await response.text();
+        if(response.ok) {
+            return { success: true, data: message }
+        }
+        return { success: false, data: message }
+    } catch (error) {
+        console.error("Error: ", error);
+        return null;
+    }
+}
+
+export async function deauthenticateUser() {
+    try {
+        const response = await fetch(`${BASE_URL}/auth/logout`, {
+            method: 'POST',
+            credentials: "include"
+        });
+        return response.ok;
+    } catch (error) {
+        console.error("Deauthentication error: ", error);
+        return false;
+    }
+}
+
+export async function checkAuth() {
+    try {
+        const response = await fetch(`${BASE_URL}/auth/check`, {
+            method: 'GET',
+            credentials: "include"
+        });
+        if (response.ok) {
+            const user = await response.json();
+            return user;
+        } else {
+            throw new Error("You need to be logged in");
+        }
+    } catch (error) {
+        console.error("Error: ", error);
+        return null;
+    }
+}
