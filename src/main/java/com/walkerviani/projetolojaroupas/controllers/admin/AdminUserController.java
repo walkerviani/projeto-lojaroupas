d875a823c5bd43aaed236ce68fab9cf953efe2f9
Walker Viani <walkeryslan@gmail.com>
2026-03-14 20:25:00 -0300

    add AdminUserController for managing user operations in admin panel

---
+++ src/main/java/com/walkerviani/projetolojaroupas/controllers/admin/AdminUserController.java
@@ -0,0 +1,80 @@
+package com.walkerviani.projetolojaroupas.controllers.admin;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.walkerviani.projetolojaroupas.entities.User;
+import com.walkerviani.projetolojaroupas.services.UserService;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/admin/users")
+public class AdminUserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        return ResponseEntity.ok().body(userService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = userService.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj) {
+        obj = userService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+        obj = userService.update(id, obj);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @Transactional
+    @PatchMapping(value = "/{id}")
+    public ResponseEntity<User> updatePassword(@PathVariable Long id, @RequestBody String newPassword) {
+        String removeQuotes = newPassword.replaceAll("^\"|\"$", "").trim();
+        User obj = userService.updatePassword(id, removeQuotes);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping(value = "/{id}/check-password")
+    public ResponseEntity<Boolean> checkCurrentPassword(@PathVariable Long id, @RequestParam String password) {
+        boolean isValid = userService.checkCurrentPassword(id, password);
+        if(isValid) {
+            return ResponseEntity.ok().body(isValid);
+        }else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
+}
