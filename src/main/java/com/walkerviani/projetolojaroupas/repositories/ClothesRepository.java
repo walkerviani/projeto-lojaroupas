7d2cc120c44b9803d6895e5ffa9ca3327b897d70
Walker Viani <walkeryslan@gmail.com>
2026-03-14 20:55:49 -0300

    add AdminClothesController for managing clothes operations in admin panel; remove unused methods from ClothesRepository and ClothesService; move post, put and delete endpoints from ClothesController to AdminClothesController

--- src/main/java/com/walkerviani/projetolojaroupas/repositories/ClothesRepository.java
+++ src/main/java/com/walkerviani/projetolojaroupas/repositories/ClothesRepository.java
@@ -5,16 +5,10 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.walkerviani.projetolojaroupas.entities.Clothes;
-import com.walkerviani.projetolojaroupas.entities.enums.Color;
-import com.walkerviani.projetolojaroupas.entities.enums.Size;
 
 public interface ClothesRepository extends JpaRepository<Clothes, Long> {
 
     List<Clothes> findByNameContainingIgnoreCase(String name);
-
-    List<Clothes> findBySize(Size size);
-
-    List<Clothes> findByColor(Color color);
 
     List<Clothes> findByCategoryNameIgnoreCase(String name);
 }
