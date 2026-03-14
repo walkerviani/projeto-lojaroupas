9c908bcb2985a27dd248795f4237b3d18baf98da
Walker Viani <walkeryslan@gmail.com>
2026-03-14 18:59:25 -0300

    add AdminInterceptor to enforce authentication and authorization for admin routes

---
+++ src/main/java/com/walkerviani/projetolojaroupas/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.walkerviani.projetolojaroupas.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.walkerviani.projetolojaroupas.interceptors.AdminInterceptor;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer{
+
+    private final AdminInterceptor adminInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(adminInterceptor)
+        .addPathPatterns("/admin/**");
+    }
+}
