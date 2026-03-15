447fefba5a8137fd1771c0324b4b8103b24ccab7
Walker Viani <walkeryslan@gmail.com>
2026-03-15 09:38:47 -0300

    add AdminCategoryController for managing category operations in admin panel; move insert, delete and update endpoints to AdminCategoryController

--- src/main/java/com/walkerviani/projetolojaroupas/controllers/CategoryController.java
+++ src/main/java/com/walkerviani/projetolojaroupas/controllers/CategoryController.java
@@ -1,18 +1,12 @@
 package com.walkerviani.projetolojaroupas.controllers;
 
-import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.walkerviani.projetolojaroupas.entities.Category;
 import com.walkerviani.projetolojaroupas.services.CategoryService;
@@ -34,25 +28,6 @@ public class CategoryController {
     @GetMapping("/{id}")
     public ResponseEntity<Category> findById(@PathVariable Long id) {
         Category obj = categoryService.findById(id);
-        return ResponseEntity.ok().body(obj);
-    }
-
-    @PostMapping
-    public ResponseEntity<Category> insert(@RequestBody Category obj) {
-        obj = categoryService.insert(obj);
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
-        return ResponseEntity.created(uri).body(obj);
-    }
-
-    @DeleteMapping(value = "/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id) {
-        categoryService.delete(id);
-        return ResponseEntity.noContent().build();
-    }
-
-    @PutMapping(value = "/{id}")
-    public ResponseEntity<Category> update(@PathVariable Long id, @RequestBody Category obj) {
-        obj = categoryService.update(id, obj);
         return ResponseEntity.ok().body(obj);
     }
 }
---
+++ src/main/java/com/walkerviani/projetolojaroupas/controllers/admin/AdminCategoryController.java
@@ -0,0 +1,45 @@
+package com.walkerviani.projetolojaroupas.controllers.admin;
+
+import java.net.URI;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.walkerviani.projetolojaroupas.entities.Category;
+import com.walkerviani.projetolojaroupas.services.CategoryService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/admin/categories")
+public class AdminCategoryController {
+
+    private final CategoryService categoryService;
+
+    @PostMapping
+    public ResponseEntity<Category> insert(@RequestBody Category obj) {
+        obj = categoryService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        categoryService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Category> update(@PathVariable Long id, @RequestBody Category obj) {
+        obj = categoryService.update(id, obj);
+        return ResponseEntity.ok().body(obj);
+    }
+}
