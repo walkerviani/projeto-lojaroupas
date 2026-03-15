5b753b5f6fe7bec40eebb5a996c17f42f23be3c7
Walker Viani <walkeryslan@gmail.com>
2026-03-15 09:39:59 -0300

    add AdminStorageController for managing image operations in admin panel; move uploadImage endpoint to AdminStorageController

--- src/main/java/com/walkerviani/projetolojaroupas/controllers/StorageController.java
+++ src/main/java/com/walkerviani/projetolojaroupas/controllers/StorageController.java
@@ -1,17 +1,12 @@
 package com.walkerviani.projetolojaroupas.controllers;
 
-import java.io.IOException;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.walkerviani.projetolojaroupas.services.StorageService;
 
@@ -23,12 +18,6 @@ import lombok.RequiredArgsConstructor;
 public class StorageController {
 
     private final StorageService storageService;
-
-    @PostMapping
-    public ResponseEntity<?> uploadImage(@RequestParam("image") MultipartFile file) throws IOException{
-       String uploadImage = storageService.uploadFile(file);
-       return ResponseEntity.status(HttpStatus.OK).body(uploadImage);
-    }
     
     @GetMapping("/{fileName}")
     public ResponseEntity<?> downloadImage(@PathVariable String fileName){
---
+++ src/main/java/com/walkerviani/projetolojaroupas/controllers/admin/AdminStorageController.java
@@ -0,0 +1,29 @@
+package com.walkerviani.projetolojaroupas.controllers.admin;
+
+import java.io.IOException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.walkerviani.projetolojaroupas.services.StorageService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/admin/image")
+public class AdminStorageController {
+
+    private final StorageService storageService;
+
+    @PostMapping
+    public ResponseEntity<?> uploadImage(@RequestParam("image") MultipartFile file) throws IOException{
+       String uploadImage = storageService.uploadFile(file);
+       return ResponseEntity.status(HttpStatus.OK).body(uploadImage);
+    }
+}
