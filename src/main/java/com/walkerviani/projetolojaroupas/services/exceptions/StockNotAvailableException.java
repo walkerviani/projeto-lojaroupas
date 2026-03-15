491e6a5d236b36ac47b1c50afa2bce38f2940a48
Walker Viani <walkeryslan@gmail.com>
2026-03-15 10:50:03 -0300

    remove unused exception

--- src/main/java/com/walkerviani/projetolojaroupas/services/exceptions/StockNotAvailableException.java
+++
@@ -1,8 +0,0 @@
-package com.walkerviani.projetolojaroupas.services.exceptions;
-
-public class StockNotAvailableException extends RuntimeException{
-
-    public StockNotAvailableException(String message){
-        super(message);
-    }
-}
