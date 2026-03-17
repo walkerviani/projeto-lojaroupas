3b55dcf173f77c71ca40e4117d8023a1be04295e
Walker Viani <walkeryslan@gmail.com>
2026-03-17 11:03:04 -0300

    update admin path

--- src/main/java/com/walkerviani/projetolojaroupas/config/WebConfig.java
+++ src/main/java/com/walkerviani/projetolojaroupas/config/WebConfig.java
@@ -17,6 +17,6 @@ public class WebConfig implements WebMvcConfigurer{
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(adminInterceptor)
-        .addPathPatterns("/admin/**");
+        .addPathPatterns("/api/admin/**");
     }
 }
