45d8d4eea631d6d49363ecf664ce57dc3e932a38
Walker Viani <walkeryslan@gmail.com>
2026-03-18 08:57:09 -0300

    add findByCpf

--- src/main/java/com/walkerviani/projetolojaroupas/repositories/UserRepository.java
+++ src/main/java/com/walkerviani/projetolojaroupas/repositories/UserRepository.java
@@ -8,4 +8,5 @@ import com.walkerviani.projetolojaroupas.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+    Optional<User> findByCpf(String cpf);
 }
