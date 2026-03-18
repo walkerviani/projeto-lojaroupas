8fbba2a4bd544bd6cfba87e60f4276e66ac34a45
Walker Viani <walkeryslan@gmail.com>
2026-03-18 08:57:21 -0300

    add validation exception

---
+++ src/main/java/com/walkerviani/projetolojaroupas/services/exceptions/ValidationException.java
@@ -0,0 +1,8 @@
+package com.walkerviani.projetolojaroupas.services.exceptions;
+
+public class ValidationException extends RuntimeException {
+
+    public ValidationException(String message) {
+        super(message);
+    }
+}
