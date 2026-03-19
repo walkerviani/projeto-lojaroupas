fda59a9bb39f2eded3a4c8344b0c74e7dd78b3a7
Walker Viani <walkeryslan@gmail.com>
2026-03-19 16:59:59 -0300

    add ImageNotFound

---
+++ src/main/java/com/walkerviani/projetolojaroupas/services/exceptions/ImageNotFoundException.java
@@ -0,0 +1,8 @@
+package com.walkerviani.projetolojaroupas.services.exceptions;
+
+public class ImageNotFoundException extends RuntimeException {
+
+    public ImageNotFoundException(String message) {
+        super(message);
+    }
+}
