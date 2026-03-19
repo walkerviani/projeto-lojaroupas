1ea58f1f04010f2c2f83aa04c0d53a548b307c17
Walker Viani <walkeryslan@gmail.com>
2026-03-19 18:39:43 -0300

    improve validation and add @Transactional annotation in insert, delete and update; add validateClothes

--- src/main/java/com/walkerviani/projetolojaroupas/services/ClothesService.java
+++ src/main/java/com/walkerviani/projetolojaroupas/services/ClothesService.java
@@ -1,20 +1,23 @@
 package com.walkerviani.projetolojaroupas.services;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.walkerviani.projetolojaroupas.entities.Clothes;
 import com.walkerviani.projetolojaroupas.entities.ImageData;
+import com.walkerviani.projetolojaroupas.repositories.CategoryRepository;
 import com.walkerviani.projetolojaroupas.repositories.ClothesRepository;
+import com.walkerviani.projetolojaroupas.repositories.OrderItemRepository;
 import com.walkerviani.projetolojaroupas.repositories.StorageRepository;
 import com.walkerviani.projetolojaroupas.services.exceptions.ClothesNotFoundException;
 import com.walkerviani.projetolojaroupas.services.exceptions.DatabaseException;
+import com.walkerviani.projetolojaroupas.services.exceptions.ImageNotFoundException;
+import com.walkerviani.projetolojaroupas.services.exceptions.ValidationException;
 
-import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -23,6 +26,8 @@ public class ClothesService {
 
     private final ClothesRepository clothesRepository;
     private final StorageRepository storageRepository;
+    private final OrderItemRepository orderItemRepository;
+    private final CategoryRepository categoryRepository;
 
     public List<Clothes> findAll() {
         return clothesRepository.findAll();
@@ -41,34 +46,42 @@ public class ClothesService {
         return clothesRepository.findByCategoryNameIgnoreCase(category);
     }
 
+    @Transactional
     public Clothes insert(Clothes obj) {
-        if (obj.getImageData() != null && obj.getImageData().getName() != null) {
-            ImageData image = storageRepository.findByName(obj.getImageData().getName())
-                    .orElseThrow(() -> new RuntimeException("Image not found!"));
-            obj.setImageData(image);
+        validateClothes(obj);
+
+        if (obj.getImageData() == null) {
+            throw new ValidationException("You must select an image");
         }
+        if (obj.getImageData().getName() == null) {
+            throw new ImageNotFoundException("Image not found!");
+        }
+        ImageData image = storageRepository.findByName(obj.getImageData().getName())
+                .orElseThrow(() -> new ImageNotFoundException("Image not found!"));
+        obj.setImageData(image);
+
         return clothesRepository.save(obj);
     }
 
+    @Transactional
     public void delete(Long id) {
-        try {
-            clothesRepository.deleteById(id);
-        } catch (EmptyResultDataAccessException e) {
-            throw new ClothesNotFoundException(id);
-        } catch (DataIntegrityViolationException e) {
-            throw new DatabaseException(e.getMessage());
+        Clothes clothes = clothesRepository.findById(id)
+                .orElseThrow(() -> new ClothesNotFoundException("Clothes not found"));
+
+        if (orderItemRepository.existsByClothesId(id)) {
+            throw new DatabaseException("Cannot delete clothes: it have associated orders.");
         }
+
+        clothesRepository.delete(clothes);
     }
 
+    @Transactional
     public Clothes update(Long id, Clothes obj) {
-        try {
-            Clothes entity = clothesRepository.getReferenceById(id);
-            updateData(entity, obj);
-            return clothesRepository.save(entity);
-        } catch (EntityNotFoundException e) {
-            throw new ClothesNotFoundException(id);
-        }
-
+        Clothes entity = clothesRepository.findById(id)
+                .orElseThrow(() -> new ClothesNotFoundException("Clothes not found"));
+        validateClothes(obj);
+        updateData(entity, obj);
+        return clothesRepository.save(entity);
     }
 
     private void updateData(Clothes entity, Clothes obj) {
@@ -77,17 +90,52 @@ public class ClothesService {
         entity.setDescription(obj.getDescription());
         entity.setColor(obj.getColor());
         entity.setSize(obj.getSize());
+        entity.setCategory(obj.getCategory());
 
-        if (obj.getCategory() != null) {
-            entity.setCategory(obj.getCategory());
+        if (obj.getImageData() == null) {
+            throw new ValidationException("You must select an image");
+        }
+        if (obj.getImageData().getName() == null) {
+            throw new ImageNotFoundException("Image not found!");
         }
 
-        if (obj.getImageData() != null && obj.getImageData().getName() != null) {
+        ImageData newImage = storageRepository.findByName(obj.getImageData().getName())
+                .orElseThrow(() -> new ImageNotFoundException("Image not found!"));
 
-            ImageData newImage = storageRepository.findByName(obj.getImageData().getName())
-                    .orElseThrow(() -> new RuntimeException("Image not found!"));
+        entity.setImageData(newImage);
+    }
 
-            entity.setImageData(newImage);
-        }
+    private void validateClothes(Clothes obj) {
+        // Name validation
+        if (obj.getName() == null)
+            throw new ValidationException("Name is required");
+        if (obj.getName().length() < 3)
+            throw new ValidationException("Name is too short (Min. 3 characters)");
+        if (!obj.getName().matches("^[A-Za-zÀ-ÿ\\s]+$"))
+            throw new ValidationException("Name must not have numbers");
+
+        // Price validation
+        if (obj.getPrice() == null)
+            throw new ValidationException("Price is required");
+        if (obj.getPrice().compareTo(BigDecimal.ZERO) <= 0)
+            throw new ValidationException("Price must be greater than 0");
+
+        // Description validation
+        if (obj.getDescription() == null)
+            throw new ValidationException("Description is required");
+
+        // Size validation
+        if (obj.getSize() == null)
+            throw new ValidationException("You must select a valid size");
+
+        // Color validation
+        if (obj.getColor() == null)
+            throw new ValidationException("You must select a valid color");
+
+        // Category validation
+        if (obj.getCategory() == null)
+            throw new ValidationException("You must select a valid category");
+        categoryRepository.findById(obj.getCategory().getId())
+                .orElseThrow(() -> new ValidationException("You must select a valid category"));
     }
 }
