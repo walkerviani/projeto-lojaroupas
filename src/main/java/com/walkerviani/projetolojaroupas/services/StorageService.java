50a2884a95b0994a909bf8e24522efb7c7fc1f2a
Walker Viani <walkeryslan@gmail.com>
2026-03-19 17:04:19 -0300

    improve validation and IOException handling in uploadFile; rename dbImageData to imageData, change the type from Optional to ImageData and imageData now throws ImageNotFounException

--- src/main/java/com/walkerviani/projetolojaroupas/services/StorageService.java
+++ src/main/java/com/walkerviani/projetolojaroupas/services/StorageService.java
@@ -1,7 +1,6 @@
 package com.walkerviani.projetolojaroupas.services;
 
 import java.io.IOException;
-import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
@@ -9,6 +8,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.walkerviani.projetolojaroupas.entities.ImageData;
 import com.walkerviani.projetolojaroupas.repositories.StorageRepository;
+import com.walkerviani.projetolojaroupas.services.exceptions.ImageNotFoundException;
+import com.walkerviani.projetolojaroupas.services.exceptions.ValidationException;
 import com.walkerviani.projetolojaroupas.util.ImageUtils;
 
 import lombok.RequiredArgsConstructor;
@@ -19,22 +20,39 @@ public class StorageService {
 
     private final StorageRepository storageRepository;
 
-    public String uploadFile(MultipartFile file) throws IOException {
-        String randomId = UUID.randomUUID().toString();
-        String fileName = randomId + "_" + file.getOriginalFilename();
-        ImageData imageData = storageRepository.save(ImageData.builder()
-                .name(fileName)
-            .type(file.getContentType())
-        .imageData(ImageUtils.compressImage(file.getBytes())).build());
-        if(imageData != null){
-            return fileName;
+    public String uploadFile(MultipartFile file) {
+        if (file == null || file.isEmpty()) {
+            throw new ValidationException("File cannot be empty");
         }
-        return null;
+
+        if (!"image/png".equals(file.getContentType())) {
+            throw new ValidationException("Only PNG images are allowed");
+        }
+        if(file.getOriginalFilename() == null) {
+            throw new ValidationException("Invalid file name");
+        }
+        try {
+            String randomId = UUID.randomUUID().toString();
+            String fileName = randomId + "_" + file.getOriginalFilename();
+
+            storageRepository.save(ImageData.builder()
+                    .name(fileName)
+                    .type(file.getContentType())
+                    .imageData(ImageUtils.compressImage(file.getBytes())).build());
+
+            return fileName;
+        } catch (IOException e) {
+            throw new ValidationException("Error processing image file");
+        }
+
     }
 
-    public byte[] downloadImage(String fileName){
-        Optional<ImageData> dbImageData = storageRepository.findByName(fileName);
-        byte[] images = ImageUtils.decompressImage(dbImageData.get().getImageData());
+    public byte[] downloadImage(String fileName) {
+        ImageData imageData = storageRepository.findByName(fileName)
+                .orElseThrow(() -> new ImageNotFoundException("Image not found"));
+
+        byte[] images = ImageUtils.decompressImage(imageData.getImageData());
+
         return images;
     }
 }
