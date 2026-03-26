f923f4e074babef7686bdc677e279285b96b1053
Walker Viani <walkeryslan@gmail.com>
2026-03-26 15:07:16 -0300

    remove flyway .sql files

--- src/main/resources/db/migration/V2__insert_initial_data.sql
+++
@@ -1,5 +0,0 @@
--- Categories
-INSERT INTO category (name) VALUES ('Shirt');
-INSERT INTO category (name) VALUES ('Skirt');
-INSERT INTO category (name) VALUES ('Coat');
-INSERT INTO category (name) VALUES ('Pants');
